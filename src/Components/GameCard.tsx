ee1d0937e22c819f6adb99d48ac62d5c24175821
Imteaz1998 <ahmed1709032@stud.kuet.ac.bd>
2023-04-17 00:02:25 +0600

    Show Loading skeletons

--- src/Components/GameCard.tsx
+++ src/Components/GameCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+ 
 import { Game } from '../hooks/useGame'
 import { Card, CardBody, HStack, Heading, Image, Text } from '@chakra-ui/react'
 import PlatrformIconList from './PlatrformIconList'
@@ -10,7 +10,7 @@ interface Props {
 
 const GameCard = ({game}:Props) => {
   return (
-   <Card borderRadius={10} overflow='hidden'>
+   <Card  borderRadius={10} overflow='hidden'>
     <Image src={getCroppedImageUrl(game.background_image)}></Image>
     <CardBody>
         <Heading fontSize='2xl'>{game.name}</Heading>
