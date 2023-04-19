01c41aabdc90e7c603614507f2c7c088aaf1e1f5
Imteaz1998 <ahmed1709032@stud.kuet.ac.bd>
2023-04-19 14:50:31 +0600

    Highlight the selected Genre

--- src/Components/GenreList.tsx
+++ src/Components/GenreList.tsx
@@ -1,10 +1,16 @@
 
-import { HStack, Image, List, ListItem, Spinner, Text } from '@chakra-ui/react';
+import { Button, HStack, Image, List, ListItem, Spinner, Text } from '@chakra-ui/react';
 import useGenres from '../hooks/useGenre';
 import getCroppedImageUrl from '../Services/image-url';
+import { Genre } from '../hooks/useGenre';
+
+interface Props {
+  onSelectGenre: (genre:Genre)=>void;
+  selectedGenre:Genre | null;
+}
 
 
-const GenreList = () => {
+const GenreList = ({selectedGenre,onSelectGenre}: Props) => {
     const {data,isLoading,error}=useGenres();
     if (error) return null;
     if(isLoading) return <Spinner/>
@@ -13,7 +19,8 @@ const GenreList = () => {
         {data .map(genre=><ListItem key={genre.id} paddingY='5px'>
           <HStack>
             <Image boxSize='32px' borderRadius={8} src={getCroppedImageUrl(genre.image_background)}/>
-            <Text fontSize='lg'>{genre.name}</Text>
+          
+            <Button fontWeight={genre.id===selectedGenre?.id?'bold':'normal'} onClick={()=>onSelectGenre(genre)} fontSize='lg' variant='link'>{genre.name}</Button>
 
           </HStack>
           
