047b7ba2f65f9a0e8a65bfbba74823b221299de9
Imteaz1998 <ahmed1709032@stud.kuet.ac.bd>
2023-04-20 23:39:12 +0600

    cleanup the game cards

--- src/Components/GenreList.tsx
+++ src/Components/GenreList.tsx
@@ -16,13 +16,13 @@ const GenreList = ({selectedGenre,onSelectGenre}: Props) => {
     if(isLoading) return <Spinner/>
   return (
     <>
-    <Heading fontSize='2xl' marginBottom={3}>Genres</Heading>
+    <Heading fontSize='2xl' marginTop={9} marginBottom={3}>Genres</Heading>
     <List>
         {data .map(genre=><ListItem key={genre.id} paddingY='5px'>
           <HStack>
             <Image boxSize='32px' borderRadius={8} objectFit='cover' src={getCroppedImageUrl(genre.image_background)}/>
           
-            <Button whiteSpace={'normal'} textAlign={'left'} fontWeight={genre.id===selectedGenre?.id?'bold':'normal'} onClick={()=>onSelectGenre(genre)} fontSize='md'marginLeft='15px' variant='link'>{genre.name}</Button>
+            <Button whiteSpace={'normal'} textAlign={'left'} fontWeight={genre.id===selectedGenre?.id?'bold':'normal'} onClick={()=>onSelectGenre(genre)} fontSize='md' variant='link'>{genre.name}</Button>
 
           </HStack>
           
