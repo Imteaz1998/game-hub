349f3c0035810ea837f3455b5001fdc4959dc51c
Imteaz1998 <ahmed1709032@stud.kuet.ac.bd>
2023-04-21 00:11:23 +0600

    Ship Genre with APp

--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -1,10 +1,11 @@
 
 import useData from "./useData";
+import genre from "../Data/genre";
 
 export interface Genre{
     id:number;
     name: string;
-    image_background: string;
+    image_background: string; 
 }
-const useGenres=()=>useData<Genre>('/genres');
+const useGenres=()=>({data:genre,isLoading:false,error:null});
 export default useGenres
